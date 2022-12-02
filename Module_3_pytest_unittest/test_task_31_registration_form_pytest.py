91d7cf925fb486aed54db8e8980f82b963a26413
enemyOfProgress <vorobiov.bohdan@gmail.com>
2022-12-02 11:10:30 +0200

    декомпозировал задачу. Весь повторяющийся код вынес в отдельные функции, чтобы потом эти функции можно было вызвать в любом тесте

--- Module_3_pytest_unittest/test_task_31_registration_form_pytest.py
+++ Module_3_pytest_unittest/test_task_31_registration_form_pytest.py
@@ -4,71 +4,45 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+browser = webdriver.Chrome()
 
-def test_registration_form_1():
-    link = "http://suninjuly.github.io/registration1.html"
-    browser = webdriver.Chrome()
-    browser.get(link)
 
-    # Ваш код, который заполняет обязательные поля
+def fill_registration_field():
     first_name_filed = browser.find_element(By.CSS_SELECTOR, ".first_block .form-group .form-control.first")
     first_name_filed.send_keys("Ivan")
-
     last_name_field = browser.find_element(By.CSS_SELECTOR, ".first_block .form-group .form-control.second")
     last_name_field.send_keys("Ivanov")
-
     email_field = browser.find_element(By.CSS_SELECTOR, ".first_block .form-group .form-control.third")
     email_field.send_keys("test@gmail.com")
 
-    # Отправляем заполненную форму
+
+def submit_button_click():
     button = browser.find_element(By.CSS_SELECTOR, "button.btn")
     button.click()
 
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
 
-    # находим элемент, содержащий текст
+def check_successfully_registration():
     welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
     welcome_text = welcome_text_elt.text
-    time.sleep(5)
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
     assert welcome_text == "Congratulations! You have successfully registered!", "Should be absolute value of a number"
+
+
+def test_registration_form_1():
+    link = "http://suninjuly.github.io/registration1.html"
+    browser.get(link)
+    fill_registration_field()
+    submit_button_click()
+    time.sleep(5)
+    check_successfully_registration()
 
 
 def test_registration_form_2():
     link = "http://suninjuly.github.io/registration2.html"
-    browser = webdriver.Chrome()
     browser.get(link)
-
-    # Ваш код, который заполняет обязательные поля
-    first_name_filed = browser.find_element(By.CSS_SELECTOR, ".first_block .form-group .form-control.first")
-    first_name_filed.send_keys("Ivan")
-
-    last_name_field = browser.find_element(By.CSS_SELECTOR, ".first_block .form-group .form-control.second")
-    last_name_field.send_keys("Ivanov")
-
-    email_field = browser.find_element(By.CSS_SELECTOR, ".first_block .form-group .form-control.third")
-    email_field.send_keys("test@gmail.com")
-
-    # Отправляем заполненную форму
-    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
-    button.click()
-
-    # Проверяем, что смогли зарегистрироваться
-    # ждем загрузки страницы
-    time.sleep(1)
-
-    # находим элемент, содержащий текст
-    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
-    # записываем в переменную welcome_text текст из элемента welcome_text_elt
-    welcome_text = welcome_text_elt.text
+    fill_registration_field()
+    submit_button_click()
     time.sleep(5)
-
-    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
-    assert welcome_text == "Congratulations! You have successfully registered!", "Should be absolute value of a number"
+    check_successfully_registration()
 
 
 if __name__ == "__main__":
